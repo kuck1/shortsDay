836b7bc8fb7e2e1ce6353964a3657c445d579eb7
Ryan Kuck <ryan@mistplay.com>
2022-07-14 15:35:53 -0400

    project started with default compose activity

---
+++ app/src/main/java/com/kuck/shortsday/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.kuck.shortsday
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.kuck.shortsday.ui.theme.ShortsDayTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ShortsDayTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ShortsDayTheme {
+        Greeting("Android")
+    }
+}
